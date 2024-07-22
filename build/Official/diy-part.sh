794be63cd984b97eb3319b67e4eb4d18e5d2cd7e
lq-wq <61339326+lq-wq@users.noreply.github.com>
2024-07-22 11:41:11 +0800

    Update diy-part.sh

--- build/Official/diy-part.sh
+++ build/Official/diy-part.sh
@@ -68,7 +68,7 @@ export Cancel_running="0"                   # 取消路由器每天跑分任务(
 git clone https://github.com/sirpdboy/chatgpt-web.git package/luci-app-chatgpt      # chatgpt-web
 git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-app-kucat   # kucat主题
 git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design # design主题
-git clone https://github.com/animegasan/luci-app-quickstart.git package/luci-app-quickstart   # luci-app-quickstart
+git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quickstart   # luci-app-quickstart
 
 #添加bypass插件
 git clone -b bypass https://github.com/shidahuilang/openwrt-package.git package/luci-app-bypass
