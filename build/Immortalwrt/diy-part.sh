59d8d9af12c8c964c777f2863feb3a1aa4a73dbc
lq-wq <61339326+lq-wq@users.noreply.github.com>
2024-10-30 11:19:11 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -67,9 +67,6 @@ export Cancel_running="0"                    # 取消路由器每天跑分任务
 # 添加APP插件
 git clone https://github.com/sirpdboy/chatgpt-web.git package/luci-app-chatgpt      # chatgpt-web
 git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-app-kucat   # kucat主题
-git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design # design主题
-
-
 
 # 晶晨CPU系列打包固件设置(不懂请看说明)
 export amlogic_model="s905d"
