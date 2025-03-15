776fc6aa373f53d381b47babb870167fbbf735f2
lq-wq <61339326+lq-wq@users.noreply.github.com>
2025-03-15 20:54:51 +0800

    Update diy-part.sh

--- build/Official/diy-part.sh
+++ build/Official/diy-part.sh
@@ -62,7 +62,7 @@ export Cancel_running="0"                    # 取消路由器每天跑分任务
 
 # 添加APP插件
 #git clone https://github.com/sirpdboy/chatgpt-web.git package/luci-app-chatgpt           # chatgpt-web
-#git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-app-kucat        # kucat主题
+#git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat        # kucat主题
 git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quickstart    # iStoreOS-web
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky                    # luci-app-lucky 端口转发
 git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp       # 一键自动格式化分区、扩容、自动挂载插件
